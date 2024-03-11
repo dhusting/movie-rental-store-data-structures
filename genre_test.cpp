716d1e33a0435fafe87b851e3f59c86c189be6bd
= <ajplumlee33@gmail.com>
2024-03-11 15:06:38 -0700

    add test driver & test data files

---
+++ genre_test.cpp
@@ -0,0 +1,23 @@
+// -------------------------------genre_test.cpp--------------------------------
+// Team Blockbuster - CS502A
+// Created 20240311
+// Modified 20240311
+// -----------------------------------------------------------------------------
+// Summary - Test file
+// Assumptions - N/A
+// -----------------------------------------------------------------------------
+#include "inventory.h"
+#include "genre.h"
+using namespace std;
+
+int main() {
+    // instantiate inventory
+    Inventory inventory;
+    inventory.commandInputFromFile("test_config.txt");
+    inventory.commandInputFromFile("data4movies.txt");
+    inventory.commandInputFromFile("data4customers.txt");
+
+    // run commands
+    inventory.commandInputFromFile("data4commands.txt");
+    return 0;
+}
