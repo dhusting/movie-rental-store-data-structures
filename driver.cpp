405d0419ecc07f9205da6c668c54048ce7758ab0
Darren Husting <darrenhusting@gmail.com>
2024-03-11 23:38:11 -0700

    More fixes

--- driver.cpp
+++ driver.cpp
@@ -6,11 +6,11 @@ int main() {
 
     Inventory i("Test", "1122 NE 12th St Philadelphia, PA 11223");
 
-    i.commandInputFromFile("/home/dhusting/CSS502/Assignments/Assignment4/data4products.txt");
-    i.commandInputFromFile("/home/dhusting/CSS502/Assignments/Assignment4/data4genres.txt");
-    i.commandInputFromFile("/home/dhusting/CSS502/Assignments/Assignment4/data4movies.txt");
-    i.commandInputFromFile("/home/dhusting/CSS502/Assignments/Assignment4/data4customers.txt");
-    i.commandInputFromFile("/home/dhusting/CSS502/Assignments/Assignment4/data4commands.txt");
+    i.commandInputFromFile("data4products.txt");
+    i.commandInputFromFile("data4genres.txt");
+    i.commandInputFromFile("data4movies.txt");
+    i.commandInputFromFile("data4customers.txt");
+    i.commandInputFromFile("data4commands.txt");
 
     return 0;
 }
